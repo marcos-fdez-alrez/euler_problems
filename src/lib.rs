7849646dd59e387c03f36df6da4d169ab8c8450a
marcos fdez <marcos.fdez.alrez@gmail.com>
2022-11-04 00:22:56 +0100

    move test to own folder

--- src/lib.rs
+++ src/lib.rs
@@ -54,34 +54,3 @@ pub fn problem3(input: i64) -> i64 {
 
 
 // https://projecteuler.net/problem=5
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_problem1() {
-        assert_eq!(problem1(1000), 233168);
-    }
-
-    #[test]
-    fn test_problem2() {
-        assert_eq!(problem2(), 4613732);
-    }
-
-    #[test]
-    fn test_problem3() {
-        assert_eq!(problem3(600851475143), 6857);
-    }
-
-    //#[test]
-    //fn test_problem4() {
-    //    assert_eq!(add(1, 2), 906609);
-    //}
-
-    //#[test]
-    //fn test_problem5() {
-    //    assert_eq!(add(1, 2), 232792560);
-    //}
-}
---
+++ tests/tests.rs
@@ -0,0 +1,26 @@
+use euler_problems;
+
+#[test]
+fn test_problem1() {
+    assert_eq!(euler_problems::problem1(1000), 233168);
+}
+
+#[test]
+fn test_problem2() {
+    assert_eq!(euler_problems::problem2(), 4613732);
+}
+
+#[test]
+fn test_problem3() {
+    assert_eq!(euler_problems::problem3(600851475143), 6857);
+}
+
+//#[test]
+//fn test_problem4() {
+//    assert_eq!(add(1, 2), 906609);
+//}
+
+//#[test]
+//fn test_problem5() {
+//    assert_eq!(add(1, 2), 232792560);
+//}
